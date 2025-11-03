1aca0b5084c8a318de715e6ec02ece10059b784a
Nikhil <nikhilguptasgrr542006@gmail.com>
2025-11-03 14:40:45 +0530

    login photo

--- travelAdvisor/src/pages/Login.jsx
+++ travelAdvisor/src/pages/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
       <div className="login-ui-box right-10 -top-40" />
 
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-        <div className="w-2/4 h-[90vh] flex items-end bg-[url('https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&w=600')] bg-cover bg-center rounded-lg p-10 z-50">
+        <div className="w-2/4 h-[90vh] flex items-end bg-[url('/loginpage.jpeg')] bg-cover bg-center rounded-lg p-10 z-50">
           <div>
             <h4 className="text-5xl text-white font-semibold leading-[58px]">
               Create Your <br /> Travel Stories
